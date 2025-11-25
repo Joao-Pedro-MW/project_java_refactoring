e37b48bea2333e4937ff1e38f5d4a74168e2e449
joao-pedro-mw <jpedro01230@gmail.com>
2025-11-25 20:46:25 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -1,3 +1,4 @@
+
 public class Movie {
 
     public static final int CHILDRENS = 2;
@@ -37,33 +38,14 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double thisAmount = 0;
-
-        switch (price.getPriceCode()) {
-            case REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2) {
-                    thisAmount += (daysRented - 2) * 1.5;
-                }
-                break;
-            case NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3) {
-                    thisAmount += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
         int points = 1;
 
-        if ((price.getPriceCode() == NEW_RELEASE) &&
-            (daysRented > 1)) {
+        if ((price.getPriceCode() == NEW_RELEASE)
+                && (daysRented > 1)) {
             points++;
         }
 
--- Price.java
+++ Price.java
@@ -2,4 +2,6 @@
 public abstract class Price {
 
     public abstract int getPriceCode();
+
+    public abstract double getCharge(int daysRented);
 }
