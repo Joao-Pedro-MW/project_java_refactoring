9a29de288371315e40ec22219e932514a5c8e369
joao-pedro-mw <jpedro01230@gmail.com>
2025-11-25 21:03:25 -0300

    Commit 16

--- Customer.java
+++ Customer.java
@@ -1,74 +1,52 @@
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Enumeration;
+import java.util.Vector;
 
 public class Customer {
 
     private final String name;
-    private final List<Rental> rentals = new ArrayList<>();
+    private final Vector _rentals = new Vector();
 
     public Customer(String name) {
         this.name = name;
     }
 
     public void addRental(Rental rental) {
-        rentals.add(rental);
+        _rentals.addElement(rental);
     }
 
     public String getName() {
         return name;
     }
 
+    public Enumeration getRentals() {
+        return _rentals.elements();
+    }
+
     public String statement() {
-        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
-
-        for (Rental rental : rentals) {
-            result.append("\t")
-                    .append(rental.getMovie().getTitle())
-                    .append("\t")
-                    .append(String.valueOf(rental.getCharge()))
-                    .append("\n");
-        }
-
-        result.append("Amount owed is ").append(String.valueOf(getTotalCharge())).append("\n");
-        result.append("You earned ").append(String.valueOf(getTotalFrequentRenterPoints()))
-                .append(" frequent renter points");
-
-        return result.toString();
+        return new TextStatement().value(this);
     }
 
     public String htmlStatement() {
-        StringBuilder result = new StringBuilder();
-
-        result.append("<H1>Rentals for <EM>").append(getName()).append("</EM></H1><P>\n");
-
-        for (Rental rental : rentals) {
-            result.append(rental.getMovie().getTitle())
-                    .append(": ")
-                    .append(String.valueOf(rental.getCharge()))
-                    .append("<BR>\n");
-        }
-
-        result.append("<P>You owe <EM>").append(String.valueOf(getTotalCharge())).append("</EM><P>\n");
-        result.append("On this rental you earned <EM>")
-                .append(String.valueOf(getTotalFrequentRenterPoints()))
-                .append("</EM> frequent renter points<P>");
-
-        return result.toString();
+        return new HtmlStatement().value(this);
     }
 
-    private double getTotalCharge() {
+    public double getTotalCharge() {
         double result = 0;
-        for (Rental rental : rentals) {
-            result += rental.getCharge();
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
         }
         return result;
     }
 
-    private int getTotalFrequentRenterPoints() {
+    public int getTotalFrequentRenterPoints() {
         int result = 0;
-        for (Rental rental : rentals) {
-            result += rental.getFrequentRenterPoints();
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
